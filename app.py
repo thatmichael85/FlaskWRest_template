c86fa4379722cdb0d4bceb91610b3abef8e0c716
Michael <michael@Michaels-MacBook-Pro.local>
2019-02-28 09:07:20 +0800

    added pipenv and others

---
+++ app.py
@@ -0,0 +1,26 @@
+#imports
+from flask import Flask, render_template, request
+from flask_restful import Resource, Api
+
+#Create the app
+app = Flask(__name__) #create flask app
+api = Api(app) #create restful
+
+#Routes - for rendering HTML documents
+@app.route("/")
+def indexHTML():
+    return render_template("index.html")
+
+#Rest APIs
+class helloWorld(Resource):
+    def get(self):
+        return {'Hello': 'World'}
+
+#Helper Functions
+
+#main
+if __name__ == "__main__":
+    #Add rest API and define its route
+    api.add_resource(helloWorld,'/getHelloWorld') # you can access this resource through <baseURL>:<port>/getHelloWorld
+    #i kept debug at true for dev purpose. set to false when in prod.
+    app.run(host="0.0.0.0", port=8001,debug=True)
