1eeff8eb6dd8258779e9e2b37b29d1fb46933559
Olimpo, Michael <molimpo@visa.com>
2020-06-25 09:55:48 +0800

    Updated naming

--- app.py
+++ app.py
@@ -8,19 +8,18 @@ api = Api(app) #create restful
 
 #Routes - for rendering HTML documents
 @app.route("/")
-def indexHTML():
+def index():
     return render_template("index.html")
 
 #Rest APIs
-class helloWorld(Resource):
+class HelloWorld(Resource):
     def get(self):
         return {'Hello': 'World'}
 
-#Helper Functions
 
 #main
 if __name__ == "__main__":
     #Add rest API and define its route
-    api.add_resource(helloWorld,'/getHelloWorld') # you can access this resource through <baseURL>:<port>/getHelloWorld
+    api.add_resource(HelloWorld,'/getHelloWorld') # you can access this resource through <baseURL>:<port>/getHelloWorld
     #i kept debug at true for dev purpose. set to false when in prod.
     app.run(host="0.0.0.0", port=8001,debug=True)
